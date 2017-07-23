a2b3162467e9731ea26504fac7edbfc39efd243c
KomalJagdev <komaljagdev@gmail.com>
2017-07-23 12:30:32 +0530

    Add files via upload

---
+++ web_scraper.py
@@ -0,0 +1,21 @@
+## 
+##Python script to scrape all urls from a website and save it into a csv file.
+##
+
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+url = input("Enter a website:")
+r = requests.get(url)                   
+data = r.content
+soup = BeautifulSoup(data, 'html5lib')
+print(soup)
+
+with open('web_scraper.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for link in soup.find_all('a'):
+        csvwriter.writerow([link.get('href')])
+
+
